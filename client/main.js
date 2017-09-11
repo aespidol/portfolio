41329a12ac701e7063393c8ddc580e6ba0c37c28
aespidol <aespidol@gmail.com>
2017-09-11 14:33:21 -0700

    mobile scroll animation

--- client/main.js
+++ client/main.js
@@ -7,7 +7,10 @@ new WOW().init();
 var fill = false;
 var about;
 var oldHTML;
-document.addEventListener('wheel', function(el){
+var animate = true;
+
+function handleScroll(el){
+    alert("hey")
     var card = document.getElementById("card");
     var scroller = document.getElementById("scroller");
     var description = document.getElementById("description");
@@ -30,6 +33,32 @@ document.addEventListener('wheel', function(el){
         card.children[0].className = ""        
         scroller.className = scroller.className.replace(" hide", "");
         description.style = "opacity: 0;"        
+    }
+}
+document.addEventListener('wheel', handleScroll);
+document.addEventListener('touchmove', function(el){
+    var top  = window.pageYOffset || document.documentElement.scrollTop;
+    var card = document.getElementById("card");
+    var scroller = document.getElementById("scroller");
+    var description = document.getElementById("description");
+    var hideIdx = scroller.className.indexOf("hide");
+    if(top > 1 && animate){
+        animate = false;        
+        card.children[0].className = "pull"
+        setTimeout(function(){
+            card.style = "width: 100%;"            
+            description.style = "opacity: 1.0;"
+        }, 110)
+
+        if(hideIdx === -1){
+            scroller.className+=" hide";
+        }
+    } else if(top < 10 && !animate){
+        card.style = "width: 50%;"
+        card.children[0].className = ""        
+        scroller.className = scroller.className.replace(" hide", "");
+        description.style = "opacity: 0;"       
+        animate = true; 
     }
 })
 
